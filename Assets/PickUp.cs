d1c3a6e97bb9539a231b1d1915ff9bddf248f0a2
Chouqi <ines.jerome.chouqi@gmail.com>
2017-04-14 22:45:24 +0200

    coucou il y a du code qui bug pas mais pas encore fonctionnel <3 pas toucher

---
+++ Assets/PickUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PickUp : MonoBehaviour {
+
+
+	public string itemName;
+	public Collider[] colliders;
+	public bool pikedUp;
+	public float height = 1;
+
+	public void SetColliders(bool _enable){
+		foreach(Collider c in colliders){
+			c.enabled = _enable;
+		}
+	}
+}
