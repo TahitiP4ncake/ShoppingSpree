c2f4d1b0a71d07c41b45000145a945cba6215df2
TahitiP4ncake <dorian.pancake@gmail.com>
2017-06-13 21:26:38 +0200

    pick up + ui + controller

--- Assets/Scripts/ObjetPickUp.cs
+++ Assets/Scripts/ObjetPickUp.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class ObjetPickUp : MonoBehaviour {
 
@@ -10,12 +11,44 @@ public class ObjetPickUp : MonoBehaviour {
     private float position;
     public GameObject coursesTransform;
 
+    public CoursesManager manager;
+
     public List<GameObject> Courses = new List<GameObject>();
+
+    public float Price;
+    public Text priceText;
+
+    private float vegP;
+    private float canP;
+    private float botP;
+
+    private float origin;
+    public AnimationCurve priceCurve;
+    private float priceAnima;
 
     void Start()
     {
-        position = transform.position.y;
+        origin = priceText.transform.position.y;
+        priceText.text = "";
+        position = coursesTransform.transform.position.y;
         Courses.Add(coursesTransform);
+        vegP = Random.Range(2, 10)-0.01f;
+        canP = Random.Range(10, 20) - 0.01f;
+        botP = Random.Range(5, 15) - 0.01f;
+    }
+
+    void Update()
+    {
+        if (priceAnima > 0)
+        {
+            priceText.transform.position = new Vector3(priceText.transform.position.x, origin + priceCurve.Evaluate(priceAnima), priceText.transform.position.z);
+            priceAnima += Time.deltaTime;
+            if (priceCurve.Evaluate(priceAnima) <= 0)
+            {
+                priceAnima = 0;
+                
+            }
+        }
     }
 
     void OnCollisionEnter(Collision other)
@@ -25,17 +58,66 @@ public class ObjetPickUp : MonoBehaviour {
         {
             rayon.PickedUp();
             
-            objet = Instantiate(rayon.objet, new Vector3(coursesTransform.transform.position.x, (rayon.objet.GetComponentInChildren<Renderer>().bounds.extents.y)*2 + position, coursesTransform.transform.position.z),transform.rotation);
+            objet = Instantiate(rayon.objet, new Vector3(coursesTransform.transform.position.x, (rayon.objet.GetComponentInChildren<Renderer>().bounds.extents.y) + position, coursesTransform.transform.position.z),transform.rotation);
             Courses.Add(objet as GameObject);
-            position += (rayon.objet.GetComponentInChildren<Renderer>().bounds.extents.y)*2;
+            position += (rayon.objet.GetComponentInChildren<Renderer>().bounds.extents.y);
             //objet.transform.SetParent(gameObject.transform);
             objet.GetComponent<dragObject>().papa = Courses[Courses.Count - 2].transform;
             objet.GetComponent<dragObject>().index = Courses.Count;
+            SetLayerRecursively(objet, 8);
+            objet.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+            if(objet.tag ==manager.carottesG.tag)
+            {
+                manager.carottesO += 1;
+                manager.UpdateListe();
+            }
+            if (objet.tag == manager.tomatesG.tag)
+            {
+                manager.tomatesO += 1;
+                manager.UpdateListe();
+            }
+            if (objet.tag == manager.sodaG.tag)
+            {
+                manager.sodaO += 1;
+                manager.UpdateListe();
+            }
+            if (objet.tag == manager.ananasG.tag)
+            {
+                manager.ananasO += 1;
+                manager.UpdateListe();
+            }
+            if (objet.tag == manager.cassouletG.tag)
+            {
+                manager.cassouletO += 1;
+                manager.UpdateListe();
+            }
+            if (objet.tag == manager.poisG.tag)
+            {
+                manager.poisO += 1;
+                manager.UpdateListe();
+            }
+            manager.ObjectOnTheList(objet);
         }
     }
 
-    void Update()
+    public static void SetLayerRecursively(GameObject go, int layerNumber)
     {
-        //Debug.Log(Courses);
+        foreach (Transform trans in go.GetComponentsInChildren<Transform>(true))
+        {
+            trans.gameObject.layer = layerNumber;
+        }
+    }
+
+    public void WinScreen()
+    {
+        Price = manager.carottesO * vegP +
+                manager.tomatesO * vegP +
+                manager.sodaO * botP +
+                manager.ananasO * canP +
+                manager.cassouletO * canP +
+                manager.poisO * canP;
+        priceText.text = Mathf.Floor(Price*100)/100 + " €";
+        priceAnima = 0.001f;
+
     }
 }
--- Assets/Scripts/dragObject.cs
+++ Assets/Scripts/dragObject.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class dragObject : MonoBehaviour {
 
@@ -12,15 +10,16 @@ public class dragObject : MonoBehaviour {
 
     void Start()
     {
+        transform.rotation = Quaternion.Euler(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
         origin = transform.rotation;
     }
 	void FixedUpdate () {
         if (papa != null)
         {
             //transform.position = Vector3.Lerp(transform.position, new Vector3(papa.position.x, transform.position.y, papa.position.z), 0.5f);
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(papa.position.x, transform.position.y, papa.position.z), ref velocity, 0.01f*index/5);
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(papa.position.x, transform.position.y, papa.position.z), ref velocity, 0.01f*index/10);
             //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(papa.position.x, papa.position.y+GetComponentInChildren<Renderer>().bounds.extents.y, papa.position.z), ref velocity, 0.01f * index / 5);
-            //transform.rotation = papa.rotation*origin;
+            transform.rotation = papa.rotation*origin;
             //.GetComponentInChildren<Renderer>().bounds.extents.y)
         }
 	}
